517fb856f34bb95553f0f1d0f2dfe2e4242d0fb6
nerdyraspberry <107356356+nerdyraspberry@users.noreply.github.com>
2025-09-08 12:03:06 +0200

    nu met IMU, enigsinds... kan draaiing bepalen, geen positie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MeasuringAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MeasuringAutonomous.java
@@ -3,12 +3,15 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.kowalski.control.PositionDeterminer;
 import org.firstinspires.ftc.teamcode.kowalski.systems.MecanumDrive;
 import org.firstinspires.ftc.teamcode.kowalski.systems.MecanumMotor;
 
 @Autonomous(name = "Measuring Autonomous")
 public class MeasuringAutonomous extends LinearOpMode {
     private MecanumDrive drivetrain;
+    private PositionDeterminer pd;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -24,13 +27,22 @@ public class MeasuringAutonomous extends LinearOpMode {
                 .setSigns(MecanumMotor.BackLeft, "+", "-", "-")
                 .setSigns(MecanumMotor.BackRight, "-", "-", "+");
 
+        pd = new PositionDeterminer(hardwareMap);
+        pd.initialize();
+
         waitForStart();
 
         drivetrain
                 .move(0.0, 0.0)
-                .rotate(0.6)
+                .rotate(0.2)
                 .apply();
-        sleep(500);
+        while (opModeIsActive()) {
+            telemetry.addData("angle", pd.getAngle());
+            telemetry.update();
+
+            if (pd.getAngle() >= 80.0 && pd.getAngle() <= 90.0)
+                break;
+        }
         drivetrain.move(0.0, 0.0)
                 .rotate(0.0)
                 .apply();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kowalski/control/PositionDeterminer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kowalski/control/PositionDeterminer.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.kowalski.control;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 public class PositionDeterminer {
+    private IMU imu;
+
+    public PositionDeterminer(HardwareMap hardwareMap) {
+        IMU.Parameters imuParams =
+                new IMU.Parameters(new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP
+                ));
+
+        this.imu = hardwareMap.get(IMU.class, "imu");
+        this.imu.initialize(imuParams);
+
+    }
+
+    /** Reset everything! */
+    public void initialize() {
+        this.imu.resetYaw();
+    }
+
+    /** Get the angle that the robot is currently pointing in */
+    public double getAngle() {
+        return -this.imu
+                .getRobotYawPitchRollAngles()
+                .getYaw(AngleUnit.DEGREES);
+    }
 }
