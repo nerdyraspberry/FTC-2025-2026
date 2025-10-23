4a5e8c16fbabf61bd95ad9c6689b7322258c2353
nerdyraspberry <107356356+nerdyraspberry@users.noreply.github.com>
2025-10-23 12:03:42 +0200

    Autonomous code when starting in Scoring Area

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CommonAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CommonAutonomous.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.Alliance;
 import org.firstinspires.ftc.teamcode.SharedThingemajigs;
@@ -14,6 +15,7 @@ public class CommonAutonomous {
             LinearOpMode opMode
     ) {
         MecanumDrive drivetrain = SharedThingemajigs.makeMecanum(opMode.hardwareMap);
+        DcMotor shootingMotor = opMode.hardwareMap.get(DcMotor.class, "shootingMotor");
 
         if (startPos == StartingPosition.TRIANGLE) {
             // Move out of the triangle
@@ -21,8 +23,17 @@ public class CommonAutonomous {
                     .apply();
             opMode.sleep(1000);
             drivetrain.move(0, 0).apply();
-        } else {
-            // Don't do anything here... yet
+        } else if (startPos == StartingPosition.SCORING_AREA) {
+            // Assume we are in the right position to score, just run the ball-shooting motor.
+            shootingMotor.setPower(1.0);
+            opMode.sleep(3000);
+            shootingMotor.setPower(0.0);
+
+            // Back out of the scoring zone
+            drivetrain.move(0.0, -1.0)
+                    .apply();
+            opMode.sleep(1000);
+            drivetrain.move(0, 0).apply();
         }
     }
 }
