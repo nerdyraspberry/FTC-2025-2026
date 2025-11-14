f8deadb00f2f18bab343e7b6e315c5e6c89d52d8
nerdyraspberry <107356356+nerdyraspberry@users.noreply.github.com>
2025-11-14 15:28:13 +0100

    Variable changes, some new code that is unused for now, proper safe mode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
@@ -18,17 +18,49 @@ public class BaseTeleOp extends LinearOpMode {
     private Servo ballHoldingServo;
 
     // VARIABLES
-    final double speedChangeIncrement = .01;
-    final double maxSpeed = 1.0;
-    double speed = 0.5;
+    final double speedChangeIncrement = .05;
+    double maxSpeed = 1.0;
+    double speed;
+    boolean safeMode = false;
 
     final int speedChangeDebounce = 250;
 
     final double servoGateClose = 0.3;
-    final double servoGateOpen = 0.45;
+    final double servoGateOpen = 0.52;
 
-    final double shootingMotorOnSpeed = -1.0;
+    final double shootingMotorOnSpeed = 1.0;
+
+    final double delayBeforeLatchCanOpen = .5;
     // END VARIABLES
+
+    // NOTE: this code is not YET useful, but it may be? I'm not sure!
+    // When changing the cache size, make sure to change the amount of zeroes in the array.
+    final int motorCacheSize = 4;
+    int[] motorDeltaCache = {0, 0, 0, 0};
+    int prevMotorPos = 0;
+
+    private void _shiftCacheData() {
+        for (int i = 0; i < motorCacheSize - 1; i++)
+            motorDeltaCache[i + 1] = motorDeltaCache[i];
+    }
+    private double getMotorDelta() {
+        // Calculate motor delta
+        int motorPos = shootingMotor.getCurrentPosition();
+        int newDelta = motorPos - prevMotorPos;
+        prevMotorPos = motorPos;
+
+        // Store in cache
+        _shiftCacheData();
+        motorDeltaCache[0] = newDelta;
+
+        // Perform averaging
+        double avgMotorDelta = 0;
+        for (int i = 0; i < motorCacheSize; i++)
+            avgMotorDelta += (double)motorDeltaCache[i];
+        avgMotorDelta /= 4;
+
+        return avgMotorDelta;
+    }
 
     @Override
     public void runOpMode() {
@@ -42,8 +74,19 @@ public class BaseTeleOp extends LinearOpMode {
 
         ballHoldingServo.setDirection(Servo.Direction.REVERSE);
 
+        // Allow setting safe mode
+        if (gamepad1.right_bumper) {
+            maxSpeed = 0.5;
+            safeMode = true;
+        }
+        speed = maxSpeed;
+
         waitForStart();
 
+        boolean mayOpenLatch = false;
+        double lastSpinupTime = 0.0;
+
+        int motorPos = 0;
         while (opModeIsActive()) {
             // Move
             drivetrain.move(speed * gamepad1.left_stick_x, -speed * gamepad1.left_stick_y)
@@ -61,17 +104,37 @@ public class BaseTeleOp extends LinearOpMode {
             }
             if (gamepad1.dpad_left) speed = maxSpeed;
 
-            shootingMotor.setPower((gamepad1.x || gamepad1.square) ? shootingMotorOnSpeed : 0.0);
+            if (!safeMode) {
+                shootingMotor.setPower((gamepad1.x || gamepad1.square) ? shootingMotorOnSpeed : 0.0);
+                // Book-keeping: will prevent servo from opening if motor is not up to speed
+                if (gamepad1.x && !mayOpenLatch) {
+                    mayOpenLatch = true;
+                    lastSpinupTime = getRuntime();
+                } else if (!gamepad1.x && mayOpenLatch) {
+                    mayOpenLatch = false;
+                }
+                // Book-keeping: check the spinning speed of the motor
+                // TODO: figure out if and how this may be used
+                //telemetry.addData("Delta Pos", getMotorDelta());
 
-            ballHoldingServo.setPosition((gamepad1.y || gamepad1.triangle) ? servoGateOpen : servoGateClose);
+                double deltaOpen = getRuntime() - lastSpinupTime;
+                telemetry.addData("Pressing Y?", gamepad1.y);
+                telemetry.addData("Allowed to open?", mayOpenLatch && deltaOpen > delayBeforeLatchCanOpen);
+                telemetry.addData("Will open?", (gamepad1.y || gamepad1.triangle) && (mayOpenLatch && deltaOpen > delayBeforeLatchCanOpen));
+                telemetry.update();
+                ballHoldingServo.setPosition((gamepad1.y || gamepad1.triangle) && (mayOpenLatch && deltaOpen > delayBeforeLatchCanOpen) ? servoGateOpen : servoGateClose);
 
-            if (gamepad1.left_bumper) {
-                shootingMotor.setPower(shootingMotorOnSpeed);
-                sleep(300);
-                ballHoldingServo.setPosition(servoGateOpen);
-                sleep(3000);
-                ballHoldingServo.setPosition(servoGateClose);
-                shootingMotor.setPower(0.0);
+                if (gamepad1.left_bumper) {
+                    shootingMotor.setPower(shootingMotorOnSpeed);
+                    sleep((long)(delayBeforeLatchCanOpen * 1000.0));
+                    ballHoldingServo.setPosition(servoGateOpen);
+                    sleep(3000);
+                    ballHoldingServo.setPosition(servoGateClose);
+                    shootingMotor.setPower(0.0);
+                }
+            } else {
+                telemetry.addData("SAFE MODE", "ENABLED");
+                telemetry.update();
             }
         }
     }
